b4da54235a4f8fcf04a87de877034126c2dd1e6a
David-VS <63110701+David-VS@users.noreply.github.com>
2025-12-15 09:59:01 +0100

    added class Person

---
+++ src/be/ehb/personen/Persoon.java
@@ -0,0 +1,39 @@
+package be.ehb.personen;
+
+import java.time.LocalDate;
+
+public class Persoon {
+
+    private String firstName, lastName;
+    private LocalDate birthdate;
+
+    public Persoon(String firstName, String lastName, LocalDate birthdate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+}
